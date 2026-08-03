5f6c1a9dde63ea4c1822ba983fbd85bcdb678a07
NinjaMestre <111713517+NinjaMestre@users.noreply.github.com>
2026-08-03 17:28:21 -0300

    Riff Theory da cor certa

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,7 @@ export default function Header({ title, span, icon }) {
             <img src={icon} width={128} height={128}></img>
             {
                 split.map((item, index) => (
-                    <h1 className={index % 2 != 0 ? "primary" : "secondary"}>{item}</h1>
+                    <h1 className={index % 2 == 0 ? "primary" : "secondary"}>{item}</h1>
                 ))
             }
             <span>{span}</span>
