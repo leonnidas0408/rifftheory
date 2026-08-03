84ff336ea257feabe6632a840a8be0999a6c645d
NinjaMestre <111713517+NinjaMestre@users.noreply.github.com>
2026-08-03 16:57:13 -0300

    Melhorei o tema

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 export default function Header({ title, span }) {
+    let split = title.split(" ");
     return (
         <header>
-            <h1>{title}</h1>
+            {
+                split.map((item, index) => (
+                    <h1 className={index % 2 != 0 ? "primary" : "secondary"}>{item}</h1>
+                ))
+            }
             <span>{span}</span>
         </header>
     )
