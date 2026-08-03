1f1a17d7967c5a814fd2e5bbcc421d1314ffb8ae
NinjaMestre <111713517+NinjaMestre@users.noreply.github.com>
2026-08-03 19:07:29 -0300

    Tirei semicolon haha

--- src/components/braco.jsx
+++ src/components/braco.jsx
@@ -4,5 +4,5 @@ import { trata } from "../draw";
 export default function Braco() {
     return (
         <canvas id="braco" width="320" height="220" onClick={(e) => trata(e.clientX, e.clientY)}></canvas>
-    );
+    )
 }
