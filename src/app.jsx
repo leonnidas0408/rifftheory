14fae54dbe5bd1ccaf2e9c1401696d3447c8c04f
NinjaMestre <111713517+NinjaMestre@users.noreply.github.com>
2026-08-03 17:12:04 -0300

    Agora com ícone

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,7 @@ import Resultado from "./components/resultado";
 export default function App() {
     return (
         <div>
-            <Header title="🎸 Riff Theory" span="Escalas · Campo harmônico · Braço interativo"/>
+            <Header title="Riff Theory" span="Escalas · Campo harmônico · Braço interativo" icon="src/assets/icon.jpg"/>
             <NotaEEscala/>
             <Resultado/>
         </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function Header({ title, span }) {
+export default function Header({ title, span, icon }) {
     let split = title.split(" ");
     return (
         <header>
+            <img src={icon} width={128} height={128}></img>
             {
                 split.map((item, index) => (
                     <h1 className={index % 2 != 0 ? "primary" : "secondary"}>{item}</h1>
