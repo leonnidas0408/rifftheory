3892fa0f1a0827b37e6cf09637244ac01ea9d779
NinjaMestre <111713517+NinjaMestre@users.noreply.github.com>
2026-08-04 22:39:03 -0300

    Criei um componente que ajusta a utilização de SidebarPages e BottomBarPages com base no dispositivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { React, useState } from "react";
-import { isDesktop } from "react-device-detect";
 import Header from "./components/Header";
 import NotaEEscala from "./components/NotaEEscala";
 import Resultado from "./components/Resultado";
 import RiffTheoryTuner from "./components/pages/RiffTheoryTuner";
 import PaginaDeEscala from "./components/pages/PaginaDeEscala";
-import BottomBar from "./components/BottomBarPages";
 import Home from "./components/pages/Home";
-import SidebarPages from "./components/SidebarPages";
 import Metronomo from "./components/pages/Metronomo";
+import NavbarAdapter from "./components/NavbarAdapter";
 
 export default function App() {
     const [page, setPage] = useState("Início");
@@ -35,11 +33,7 @@ export default function App() {
     return (
         <div>
             <Header title="Riff Theory" span="Escalas · Campo harmônico · Braço interativo" icon="/icon.jpg"/>
-            {
-                isDesktop ?
-                <SidebarPages page={page} setPage={setPage} pageData={pageData} defaultPage={defaultPage}/>
-                : <BottomBar page={page} setPage={setPage} pageData={pageData} defaultPage={defaultPage}/>
-            }
+            <NavbarAdapter page={page} setPage={setPage} pageData={pageData} defaultPage={defaultPage}/>
         </div>
     )
 }
